ef6946df9cd6cc8078af10c79dd9095da64f6aff
Sanghwa Lee <jaja_spider@naver.com>
2020-08-12 13:42:19 +0900

    제품명 출력 가능하도록 설정

--- src/com/sonaapi/Croll.java
+++ src/com/sonaapi/Croll.java
@@ -1,7 +1,9 @@
 package com.sonaapi;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Croll {
@@ -18,5 +20,10 @@ public class Croll {
         // url = https://www.coupang.com/vp/products/7215502
         String url = "https://www.coupang.com/vp/products/"+this.itemcode;
         driver.get(url);
+        WebElement itemnametag = driver.findElement(By.className("prod-buy-header__title"));
+        String itemname = itemnametag.getText();
+        System.out.println(itemname);
+
+        driver.quit();
     }
 }
