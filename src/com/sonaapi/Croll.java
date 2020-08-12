83b6166d79add30f172758e3acc947626e83bc79
Sanghwa Lee <jaja_spider@naver.com>
2020-08-12 16:41:49 +0900

    상품명, 재고상태, 기존가격, 할인가격 출력

--- src/com/sonaapi/Croll.java
+++ src/com/sonaapi/Croll.java
@@ -11,6 +11,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.concurrent.TimeUnit;
 
 public class Croll {
+    private int convertstringtoint(String pricedata){
+        String intStr = pricedata.replaceAll("[^0-9]", "");
+        return Integer.parseInt(intStr);
+    }
+
     private static WebDriver driver;
     public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
     public static final String WEB_DRIVER_PATH = "chromedriver";
@@ -26,15 +31,17 @@ public class Croll {
         chromeOptions.addArguments("--no-sandbox");
         driver = new ChromeDriver(chromeOptions);
 //        driver = new ChromeDriver();
-        // url = https://www.coupang.com/vp/products/7215502
+//         url = https://www.coupang.com/vp/products/7215502
         String url = "https://www.coupang.com/vp/products/"+this.itemcode;
         driver.get(url);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        try {
-            WebElement itemnametag = driver.findElement(By.className("prod-buy-header__title"));
-            String itemname = itemnametag.getText();
-            System.out.println(itemname);
 
+        // 제품명 출력
+        WebElement itemnametag = driver.findElement(By.className("prod-buy-header__title"));
+        String itemname = itemnametag.getText();
+        System.out.println(itemname);
+        try {
+            // 재고가없으면, 재고없음 출력 / 만약 Element를 못찾는다면 catch문으로 이동하여 재고없음 출력
             WebElement itemstatustag = driver.findElement(By.className("oos-label"));
             String itemstatus = itemstatustag.getText();
             System.out.println(itemstatus);
@@ -45,6 +52,16 @@ public class Croll {
         catch(Exception e){
             e.printStackTrace();
         }
+        // 상품 원래 가격 출력
+        WebElement itempricetag = driver.findElement(By.className("origin-price"));
+        String itemprice = itempricetag.getText();
+        System.out.println(convertstringtoint(itemprice));
+
+        // 할인 후 상품 가격 출력
+        WebElement itemsalepricetag = driver.findElement(By.xpath("//*[@id=\"contents\"]/div[1]/div/div[3]/div[5]/div[1]/div/div[2]/span[1]/strong"));
+        String itemsaleprice = itemsalepricetag.getText();
+        System.out.println(convertstringtoint(itemsaleprice));
+
 
 
 
